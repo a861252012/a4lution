df567b068ba7ac35dcdf8101cb95403768308879
Yong <et79165@gmail.com>
2022-01-11 17:41:47 +0800

    fix

--- app/Models/EmployeeMonthlyFeeRule.php
+++ app/Models/EmployeeMonthlyFeeRule.php
@@ -2,16 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Facades\Auth;
 
 class EmployeeMonthlyFeeRule extends Model
 {
     protected $table = "employee_monthly_fee_rules";
 
-    protected $guarded = ['id'];
+    protected $primaryKey = null;
+
+    protected $guarded = [];
+
+    public $incrementing = false;
 
     protected $casts = [
         'rate_base' => 'float',
@@ -21,41 +22,4 @@ class EmployeeMonthlyFeeRule extends Model
         'tier_1_over_a_year' => 'float',
         'tier_2_over_a_year' => 'float',
     ];
-
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::creating(function ($exchangeRate) {
-            $exchangeRate->updated_by = Auth::id();
-            $exchangeRate->created_by = Auth::id();
-            $exchangeRate->active = 1;
-        });
-        static::updating(function ($exchangeRate) {
-            $exchangeRate->updated_by = Auth::id();
-        });
-    }
-
-    ###################
-    ## Relationships ##
-    ###################
-
-    public function roles(): BelongsTo
-    {
-        return $this->belongsTo(Role::class, 'role_id', 'id');
-    }
-
-    /**
-     * Scope a query to only include active users.
-     *
-     * @param Builder $query
-     * @return Builder
-     */
-    public function scopeActive(Builder $query): Builder
-    {
-        return $query->where('active', 1);
-    }
 }
