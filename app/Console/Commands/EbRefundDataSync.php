26f4e0104622ef125ea9779c1925c3f764820b81
tedLinContin <a861252012@gmail.com>
2022-04-20 14:33:11 +0800

    #984
    調整退款排程和修正pdf匯出計算

--- app/Console/Commands/EbRefundDataSync.php
+++ app/Console/Commands/EbRefundDataSync.php
@@ -3,59 +3,31 @@
 namespace App\Console\Commands;
 
 use App\Models\RmaRefundList;
-use App\Repositories\RmaRefundListRepository;
 use App\Support\ERPRequester;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
 class EbRefundDataSync extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'eb_refund_data_sync';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'eb_refund_data_sync';
-
     private const EB_SERVICE_NAME = 'rmaRefundList';
     private const LOG_CHANNEL = 'daily_refund_sync';
-    private RmaRefundListRepository $rmaRefundListRepository;
+    private const PAGE_SIZE = 200;
+    protected $signature = 'eb_refund_data_sync';
+    protected $description = 'eb_refund_data_sync';
     private ERPRequester $erpRequest;
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct(
-        RmaRefundListRepository $rmaRefundListRepository,
-        ERPRequester            $ERPRequest
-    )
-    {
+        ERPRequester $ERPRequest
+    ) {
         parent::__construct();
-        $this->rmaRefundListRepository = $rmaRefundListRepository;
         $this->erpRequest = $ERPRequest;
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
         //預設排程時間為UTC+8早上五點整,故以UTC時間為準 取前一天的退款資料
         $startDateTime = gmdate('Y-m-d 00:00:00');
         $endDateTime = gmdate('Y-m-d 23:59:59');
-
-        $pageSize = 200;
 
         $res = $this->erpRequest->send(
             config('services.erp.ebUrl'),
@@ -64,7 +36,7 @@ class EbRefundDataSync extends Command
                 'create_date_form' => $startDateTime,
                 'create_date_to' => $endDateTime,
                 "page" => 1,
-                "pageSize" => $pageSize
+                "pageSize" => self::PAGE_SIZE
             ],
             self::LOG_CHANNEL
         );
@@ -78,7 +50,7 @@ class EbRefundDataSync extends Command
 
         $total = (int)$res['total'];
 
-        $totalPage = (int)ceil($total / $pageSize);
+        $totalPage = (int)ceil($total / self::PAGE_SIZE);
 
         $restData = array();
 
@@ -95,7 +67,7 @@ class EbRefundDataSync extends Command
                         'create_date_form' => $startDateTime,
                         'create_date_to' => $endDateTime,
                         "page" => $i,
-                        "pageSize" => $pageSize
+                        "pageSize" => self::PAGE_SIZE
                     ],
                     self::LOG_CHANNEL
                 )['data'];
@@ -115,7 +87,7 @@ class EbRefundDataSync extends Command
         DB::beginTransaction();
         try {
             $chunkData = array_chunk($data, 2);
-            if (isset($chunkData) && !empty($chunkData)) {
+            if (!empty($chunkData)) {
                 foreach ($chunkData as $item) {
                     RmaRefundList::insert($item);
                 }
--- resources/views/invoice/pdf/fbaFirstMileShipmentFee.blade.php
+++ resources/views/invoice/pdf/fbaFirstMileShipmentFee.blade.php
@@ -95,201 +95,201 @@
     .footer .img img { width: 100%; }
 </style>
 <body>
-    <div class="header">
-        <table>
+<div class="header">
+    <table>
+        <tr>
+            <td class="col col-1 logo-img">
+                <img src="{{ "data:image/jpg;base64, " . base64_encode(file_get_contents(public_path('pictures/A4lution_logo.jpg'))) }}" alt="A4lution_logo">
+            </td>
+            <td class="col col-2"></td>
+            <td class="col col-3"></td>
+        </tr>
+        <tr>
+            <td class="col col-1"></td>
+            <td class="col col-2"></td>
+            <td class="col col-3 title">Invoice</td>
+        </tr>
+        <tr>
+            <td class="col col-1"></td>
+            <td class="col col-2"></td>
+            <td class="col col-3"></td>
+        </tr>
+        <tr>
+            <td class="col col-1"></td>
+            <td class="col col-2">Details</td>
+            <td class="col col-3"></td>
+        </tr>
+        <tr>
+            <td class="col col-1">TO</td>
+            <td class="col col-2">Invoice Number:</td>
+            <td class="col col-3">{{ $invoice->fba_shipment_invoice_no }}</td>
+        </tr>
+        <tr>
+            <td class="col col-1">{{ $invoice->client_contact }}</td>
+            <td class="col col-2">Issue Date:</td>
+            <td class="col col-3">{{ $invoice->issue_date->format('d-M-y') }}</td>
+        </tr>
+        <tr>
+            <td class="col col-1">{{ $invoice->client_company }}</td>
+            <td class="col col-2">Payment Terms:</td>
+            <td class="col col-3">{{ $invoice->payment_terms }}  days net</td>
+        </tr>
+        <tr>
+            <td class="col col-1">{{ $invoice->client_address1 . ',' }}</td>
+            <td class="col col-2"></td>
+            <td class="col col-3"></td>
+        </tr>
+        <tr>
+            <td class="col col-1">{{ $invoice->client_address2 }}</td>
+            <td class="col col-2"></td>
+            <td class="col col-3"></td>
+        </tr>
+        <tr>
+            <td class="col col-1">{{ $invoice->client_city . ',' . $invoice->client_country }}</td>
+            <td class="col col-2"></td>
+            <td class="col col-3"></td>
+        </tr>
+    </table>
+</div>
+<div class="content">
+    <table>
+        <thead>
+        <tr>
+            <th class="col col-1">NO.</th>
+            <th class="col col-2">
+                {{sprintf("Description  (for the period of %s to %s)",
+                    $invoice->report_date->format('jS M Y'),
+                    $invoice->report_date->endOfMonth()->format('jS M Y')); }}
+            </th>
+            <th class="col col-3">Unit Price</th>
+            <th class="col col-4">Quantity</th>
+            <th class="col col-5">Amount</th>
+        </tr>
+        </thead>
+        <tbody>
+        @php($total = 0)
+        @php($loopCount = 1)
+
+        {{-- 1. Contin Storage Fee start --}}
+        @php($total += round($continStorageFee, 2))
+        @php($averageCbmUsage = number_format($continStorageFee / 300, 2))
+        <tr>
+            <td class="col col-1">{{ $loopCount }}</td>
+            <td class="col col-2">Contin Storage Fee</td>
+            <td class="col col-3"></td>
+            <td class="col col-4"></td>
+            <td class="col col-5">HKD  {{ number_format($continStorageFee, 2) }}</td>
+        </tr>
+        <tr>
+            <td class="col col-1"></td>
+            <td class="col col-2">
+                {{ "Average CBM Usage: {$averageCbmUsage}" }}
+            </td>
+            <td class="col col-3">$ {{ number_format($continStorageFee, 2) }}</td>
+            <td class="col col-4">1</td>
+            <td class="col col-5"></td>
+        </tr>
+        {{-- 1. Contin Storage Fee end --}}
+
+        {{-- 2. Contin 寄FBA的頭程費用 start --}}
+        @forelse ($firstMileShipmentFees as $firstMileShipmentFee)
+            @php($total += (float)$firstMileShipmentFee->unit_price)
+            @if($loop->last)
+                @php($loopCount = $loop->count + 1)
+            @endif
             <tr>
-                <td class="col col-1 logo-img">
-                    <img src="{{ "data:image/jpg;base64, " . base64_encode(file_get_contents(public_path('pictures/A4lution_logo.jpg'))) }}" alt="A4lution_logo">
-                </td>
-                <td class="col col-2"></td>
+                <td class="col col-1">{{  $loop->iteration + 1 }}</td>
+                <td class="col col-2">FBA shipment Fee from Continental HK warehouse to Amazon FBA warehouse:</td>
                 <td class="col col-3"></td>
+                <td class="col col-4"></td>
+                <td class="col col-5">HKD  {{ number_format($firstMileShipmentFee->unit_price, 2) }}</td>
             </tr>
             <tr>
                 <td class="col col-1"></td>
-                <td class="col col-2"></td>
-                <td class="col col-3 title">Invoice</td>
+                <td class="col col-2">
+                    {{ sprintf(
+                        "Country:%s, Shipment ID:%s, SKU:%d, Shipped Qty:%d",
+                        $firstMileShipmentFee->country,
+                        $firstMileShipmentFee->shipment_id,
+                        $firstMileShipmentFee->sku,
+                        $firstMileShipmentFee->shipped_qty,
+                    ) }}
+                </td>
+                <td class="col col-3">$ {{ number_format($firstMileShipmentFee->unit_price, 2) }}</td>
+                <td class="col col-4">1</td>
+                <td class="col col-5"></td>
+            </tr>
+        @empty
+        @endforelse
+        {{-- 2. Contin 寄FBA的頭程費用 end --}}
+
+        {{-- 3. Contin 寄FBA的頭程費用 start --}}
+        @forelse ($returnHelperList as $returnHelperItem)
+            @php($total += (float)$returnHelperItem->amount_hkd)
+            <tr>
+                <td class="col col-1">{{ $loopCount + $loop->iteration }}</td>
+                <td class="col col-2">Return Helper Charges</td>
+                <td class="col col-3"></td>
+                <td class="col col-4"></td>
+                <td class="col col-5">HKD  {{ number_format($returnHelperItem->amount_hkd, 2) }}</td>
             </tr>
             <tr>
                 <td class="col col-1"></td>
-                <td class="col col-2"></td>
-                <td class="col col-3"></td>
+                <td class="col col-2">{{ $returnHelperItem->notes }}</td>
+                <td class="col col-3">$ {{ number_format($returnHelperItem->amount_hkd, 2) }}</td>
+                <td class="col col-4">1</td>
+                <td class="col col-5"></td>
             </tr>
-            <tr>
-                <td class="col col-1"></td>
-                <td class="col col-2">Details</td>
-                <td class="col col-3"></td>
-            </tr>
-            <tr>
-                <td class="col col-1">TO</td>
-                <td class="col col-2">Invoice Number:</td>
-                <td class="col col-3">{{ $invoice->fba_shipment_invoice_no }}</td>
-            </tr>
-            <tr>
-                <td class="col col-1">{{ $invoice->client_contact }}</td>
-                <td class="col col-2">Issue Date:</td>
-                <td class="col col-3">{{ $invoice->issue_date->format('d-M-y') }}</td>
-            </tr>
-            <tr>
-                <td class="col col-1">{{ $invoice->client_company }}</td>
-                <td class="col col-2">Payment Terms:</td>
-                <td class="col col-3">{{ $invoice->payment_terms }}  days net</td>
-            </tr>
-            <tr>
-                <td class="col col-1">{{ $invoice->client_address1 . ',' }}</td>
-                <td class="col col-2"></td>
-                <td class="col col-3"></td>
-            </tr>
-            <tr>
-                <td class="col col-1">{{ $invoice->client_address2 }}</td>
-                <td class="col col-2"></td>
-                <td class="col col-3"></td>
-            </tr>
-            <tr>
-                <td class="col col-1">{{ $invoice->client_city . ',' . $invoice->client_country }}</td>
-                <td class="col col-2"></td>
-                <td class="col col-3"></td>
-            </tr>
-        </table>
-    </div>
-    <div class="content">
-        <table>
-            <thead>
-                <tr>
-                    <th class="col col-1">NO.</th>
-                    <th class="col col-2">
-                        {{sprintf("Description  (for the period of %s to %s)",
-                            $invoice->report_date->format('jS M Y'),
-                            $invoice->report_date->endOfMonth()->format('jS M Y')); }}
-                    </th>
-                    <th class="col col-3">Unit Price</th>
-                    <th class="col col-4">Quantity</th>
-                    <th class="col col-5">Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                @php($total = 0)
-                @php($loopCount = 1)
+        @empty
+        @endforelse
+        {{-- 3. Contin 寄FBA的頭程費用 end --}}
 
-                {{-- 1. Contin Storage Fee start --}}
-                @php($total += round($continStorageFee, 2))
-                @php($averageCbmUsage = number_format($continStorageFee / 300, 2))
-                    <tr>
-                        <td class="col col-1">{{ $loopCount }}</td>
-                        <td class="col col-2">Contin Storage Fee</td>
-                        <td class="col col-3"></td>
-                        <td class="col col-4"></td>
-                        <td class="col col-5">HKD  {{ number_format($continStorageFee, 2) }}</td>
-                    </tr>
-                    <tr>
-                        <td class="col col-1"></td>
-                        <td class="col col-2">
-                            {{ "Average CBM Usage: {$averageCbmUsage}" }}
-                        </td>
-                        <td class="col col-3">$ {{ number_format($continStorageFee, 2) }}</td>
-                        <td class="col col-4">1</td>
-                        <td class="col col-5"></td>
-                    </tr>
-                {{-- 1. Contin Storage Fee end --}}
-
-                {{-- 2. Contin 寄FBA的頭程費用 start --}}
-                @forelse ($firstMileShipmentFees as $firstMileShipmentFee)
-                    @php($total += $firstMileShipmentFee->unit_price)
-                    @if($loop->last)
-                        @php($loopCount = $loop->count + 1)
-                    @endif
-                    <tr>
-                        <td class="col col-1">{{  $loop->iteration + 1 }}</td>
-                        <td class="col col-2">FBA shipment Fee from Continental HK warehouse to Amazon FBA warehouse:</td>
-                        <td class="col col-3"></td>
-                        <td class="col col-4"></td>
-                        <td class="col col-5">HKD  {{ number_format($firstMileShipmentFee->unit_price, 2) }}</td>
-                    </tr>
-                    <tr>
-                        <td class="col col-1"></td>
-                        <td class="col col-2">
-                            {{ sprintf(
-                                "Country:%s, Shipment ID:%s, SKU:%d, Shipped Qty:%d",
-                                $firstMileShipmentFee->country,
-                                $firstMileShipmentFee->shipment_id,
-                                $firstMileShipmentFee->sku,
-                                $firstMileShipmentFee->shipped_qty,
-                            ) }}
-                        </td>
-                        <td class="col col-3">$ {{ number_format($firstMileShipmentFee->unit_price, 2) }}</td>
-                        <td class="col col-4">1</td>
-                        <td class="col col-5"></td>
-                    </tr>
-                @empty
-                @endforelse
-                {{-- 2. Contin 寄FBA的頭程費用 end --}}
-
-                {{-- 3. Contin 寄FBA的頭程費用 start --}}
-                @forelse ($returnHelperList as $returnHelperItem)
-                    @php($total += number_format($returnHelperItem->amount_hkd, 2))
-                    <tr>
-                        <td class="col col-1">{{ $loopCount + $loop->iteration }}</td>
-                        <td class="col col-2">Return Helper Charges</td>
-                        <td class="col col-3"></td>
-                        <td class="col col-4"></td>
-                        <td class="col col-5">HKD  {{ number_format($returnHelperItem->amount_hkd, 2) }}</td>
-                    </tr>
-                    <tr>
-                        <td class="col col-1"></td>
-                        <td class="col col-2">{{ $returnHelperItem->notes }}</td>
-                        <td class="col col-3">$ {{ number_format($returnHelperItem->amount_hkd, 2) }}</td>
-                        <td class="col col-4">1</td>
-                        <td class="col col-5"></td>
-                    </tr>
-                @empty
-                @endforelse
-                {{-- 3. Contin 寄FBA的頭程費用 end --}}
-
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td class="col col-1 total">Total</td>
-                    <td class="col col-2"></td>
-                    <td class="col col-3"></td>
-                    <td class="col col-4"></td>
-                    <td class="col col-5">HKD {{ number_format($total, 2) }}</td>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-    <div class="payment-info">
-        <ul>
-            <li>Payment Method:</li>
-            <li>1) By Transfer to the following HSBC account & send copy to
-                @isset($user->payment_checker_email)
-                    <span class="email">{{ $user->payment_checker_email }}</span>,and
-                @endisset
-                <span class="email">{{ $user->email }}</span>
-            </li>
-            <li>&nbsp;&nbsp;&nbsp;&nbsp;a) Beneficiary Name: A4lution Limited</li>
-            <li>&nbsp;&nbsp;&nbsp;&nbsp;b) Beneficiary Bank: THE HONGKONG AND SHANGHAI BANKING CORPORATION LTD</li>
-            <li>&nbsp;&nbsp;&nbsp;&nbsp;c) Swift code: HSBCHKHHHKH</li>
-            <li>&nbsp;&nbsp;&nbsp;&nbsp;d) Account no.: 004-747-095693-838</li>
-            <li>&nbsp;&nbsp;&nbsp;&nbsp;e) FPS registered email: info@a4lution.com</li>
-        </ul>
-    </div>
-    <div class="footer">
-        <table>
-            <tr>
-                <td class="info">
-                    <ul>
-                        <li>A4lution Limited</li>
-                        <li><a href="mailto:info@a4lution.com">info@a4lution.com</a></li>
-                        <li class="website-url"><a href="http://www.a4lution.com">http://www.a4lution.com</a>    </li>
-                        <li>Mail box no. 621-08, Unit 621, 6/F, Building 19W,</li>
-                        <li>No. 19 Science Park West Avenue,</li>
-                        <li>Hong Kong Science Park, Pak Shek Kok, N.T., Hong Kong</li>
-                    </ul>
-                </td>
-                <td class="img">
-                    <img src="{{ "data:image/png;base64, " . base64_encode(file_get_contents(public_path('pictures/A4lution_signature.jpg'))) }}" alt="A4lution_logo">
-                </td>
-            </tr>
-        </table>
-    </div>
+        </tbody>
+        <tfoot>
+        <tr>
+            <td class="col col-1 total">Total</td>
+            <td class="col col-2"></td>
+            <td class="col col-3"></td>
+            <td class="col col-4"></td>
+            <td class="col col-5">HKD {{ number_format($total, 2) }}</td>
+        </tr>
+        </tfoot>
+    </table>
+</div>
+<div class="payment-info">
+    <ul>
+        <li>Payment Method:</li>
+        <li>1) By Transfer to the following HSBC account & send copy to
+            @isset($user->payment_checker_email)
+                <span class="email">{{ $user->payment_checker_email }}</span>,and
+            @endisset
+            <span class="email">{{ $user->email }}</span>
+        </li>
+        <li>&nbsp;&nbsp;&nbsp;&nbsp;a) Beneficiary Name: A4lution Limited</li>
+        <li>&nbsp;&nbsp;&nbsp;&nbsp;b) Beneficiary Bank: THE HONGKONG AND SHANGHAI BANKING CORPORATION LTD</li>
+        <li>&nbsp;&nbsp;&nbsp;&nbsp;c) Swift code: HSBCHKHHHKH</li>
+        <li>&nbsp;&nbsp;&nbsp;&nbsp;d) Account no.: 004-747-095693-838</li>
+        <li>&nbsp;&nbsp;&nbsp;&nbsp;e) FPS registered email: info@a4lution.com</li>
+    </ul>
+</div>
+<div class="footer">
+    <table>
+        <tr>
+            <td class="info">
+                <ul>
+                    <li>A4lution Limited</li>
+                    <li><a href="mailto:info@a4lution.com">info@a4lution.com</a></li>
+                    <li class="website-url"><a href="http://www.a4lution.com">http://www.a4lution.com</a>    </li>
+                    <li>Mail box no. 621-08, Unit 621, 6/F, Building 19W,</li>
+                    <li>No. 19 Science Park West Avenue,</li>
+                    <li>Hong Kong Science Park, Pak Shek Kok, N.T., Hong Kong</li>
+                </ul>
+            </td>
+            <td class="img">
+                <img src="{{ "data:image/png;base64, " . base64_encode(file_get_contents(public_path('pictures/A4lution_signature.jpg'))) }}" alt="A4lution_logo">
+            </td>
+        </tr>
+    </table>
+</div>
 </body>
 </html>
